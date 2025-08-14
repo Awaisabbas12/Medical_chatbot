3952140818b029afecc0091fb28af5985e1b8276
AwaisAbbas12 <Awaisabbas066@gmail.com>
2025-08-14 13:16:42 +0500

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name ="Medical Chatbot",
+    version="0.01.0",
+    author_email="Awaisabbas@gmailcom",
+    author = "Awais Abbas",
+    packages=find_packages(),
+    install_requires=[]
+)
